694f0e936666aaae0f778578c71013155f92b2d1
Lucas Ren <nichokiki@hotmail.com>
2020-04-28 17:06:39 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.WriteLine("Hello World")
+            
+        }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string getUser(){
+            return this.Name;
+        }
+    }
+}
