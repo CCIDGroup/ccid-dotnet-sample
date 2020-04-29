d1da34850a0e3b6b22ffea1fe36c17e7bb66add0
Lucas Ren <nichokiki@hotmail.com>
2020-04-29 11:09:00 +0800

    first commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace demo
 {
@@ -6,10 +7,19 @@ namespace demo
     {
         static void Main(string[] args)
         {
+            // BAD: Password is generated using a cryptographically insecure RNG
+            Random gen = new Random();
+            string password = "mypassword" + gen.Next();
+            Console.WriteLine(password);
+            // GOOD: Password is generated using a cryptographically secure RNG
+            using (var crypto = new RNGCryptoServiceProvider())
+            {
+                byte[] randomBytes = new byte[sizeof(int)];
+                crypto.GetBytes(randomBytes);
+                password = "mypassword" + BitConverter.ToInt32(randomBytes);
+            }
+            Console.WriteLine(password);
 
-            
-            Console.WriteLine("Hello World")
-            
         }
     }
 
